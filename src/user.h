c9f377f2a06c9f5b39fbabb778eeeea5f5504f6b
Philipp Clostermeyer (Renesas) <philipp.clostermeyer.ct@renesas.com>
2025-01-08 09:57:28 +0100

    commit

--- src/user.h
+++ src/user.h
@@ -9,8 +9,8 @@
 #define WIFI_SSID               "" // Name WIFI Hotspot
 #define WIFI_PWD                ""   // Password für das WIFI
 
-#define IO_USERNAME             ""  // Adafruit Username
-#define IO_PASSWORD             "" // Adafruit Key
+#define IO_USERNAME             ""  // Insert Adafruit Username
+#define IO_PASSWORD             "" // Insert Adafruit Key
 
 #define EXAMPLE_MQTT_HOST       "io.adafruit.com" // Hostname des Adafruit MQTT Brokers
 #define EXAMPLE_MQTT_PORT       1883 // Broker Port (Ohne SSL) - sonst 8883
